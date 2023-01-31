c92add3d94b11e656a9dd83125af1c67e6fe48af
rydial <kimdanny2001@gmail.com>
2023-01-31 23:07:03 +1100

    Added User Input Constants

---
+++ src/Screen/Constants/UserInputs.h
@@ -0,0 +1,34 @@
+#ifndef SRC_USER_INPUTS_H
+#define SRC_USER_INPUTS_H
+
+
+#include <cstdint>
+
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// User Inputs //////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+enum class UserInput
+{
+    /********************************* Mouse **********************************/
+
+    MOUSE_1,
+    MOUSE_2,
+    MOUSE_3,
+
+    MOUSE_LEFT      = MOUSE_1,
+    MOUSE_RIGHT     = MOUSE_2,
+    MOUSE_MIDDLE    = MOUSE_3,
+
+    /******************************** Keyboard ********************************/
+
+    KEY_LEFT,
+    KEY_RIGHT,
+    KEY_UP,
+    KEY_DOWN
+};
+
+
+#endif // SRC_USER_INPUTS_H
