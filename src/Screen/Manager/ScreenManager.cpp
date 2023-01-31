d98553f33ef0987727df52aa5d7b86e5c895545b
rydial <kimdanny2001@gmail.com>
2023-01-31 23:08:50 +1100

    Major Changes: Initializing Main Menu, Console and Pacman Screens. Added Screen Abstraction and Panel Initialization, Modification & Management.

--- src/Screen/Manager/ScreenManager.cpp
+++ src/Screen/Manager/ScreenManager.cpp
@@ -1,6 +1,9 @@
 #include "ScreenManager.h"
 
 #include "Debug.h"
+#include "ExitCodes.h"
+#include "Panels.h"
+#include "ScreenCodes.h"
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -10,17 +13,167 @@
 
 void ScreenManager::init()
 {
+    /*
+        Notes: Order of Screen Initialization Must Follow ScreenCode Order
+    */
+
+    // Initialize Console Screen
+    initConsole();
+
+    // Initialize MainMenu Screen
+    initMainMenu();
+
+    // Initialize Pacman Screen
+    initPacman();
+
+    // Set Current Screen
+    setCurrentScreen(screencode::MAINMENU);
+
     DEBUG_PRINT("Screens Initialized");
 }
 
 
-void ScreenManager::update()
+uint32_t ScreenManager::update()
 {
+    Panel& panel {getCurrentScreenPanel()};
+
+    /************************* User Input Processing **************************/
+
+    // Process User Input for Current Screen
+    uint32_t retCode {panel.processUserInput()};
+
+    switch (retCode)
+    {
+        case exitcode::EXIT_PROGRAM:        // Program Exit
+            return exitcode::EXIT_PROGRAM;
+
+        case exitcode::CONT_PROGRAM:        // No Action
+            break;
+
+        default:                            // Switch Screen
+            setCurrentScreen(retCode);
+            break;
+    }
+
+    /**************************** Screen Updating *****************************/
+
+    // Update Current Screen
+    panel.update();
+
+    /**************************************************************************/
+
+    return exitcode::CONT_PROGRAM;
+}
+
+
+void ScreenManager::fillRenderQueue() /* Work In Progress */
+{
+    // Fill Render Queue
+    getCurrentScreenPanel().render();
     
+    // if (consoleActive)
+    // {
+    //     // Retrieve Console Screen
+    //     // ...
+
+    //     // Render Console
+    //     // ...
+    // }
 }
 
 
 void ScreenManager::cleanup()
 {
     DEBUG_PRINT("Screens Cleaned");
+}
+
+/**************************** Init-Stage Functions ****************************/
+
+void ScreenManager::initConsole() /* Work In Progress */
+{
+    // Allocate Memory For Panels (Prevents Reference Invalidation)
+    uint32_t panelCount {1};
+    panels.reserve(panels.size() + panelCount);
+
+    /******************************* Main Panel *******************************/
+
+    // Initialize Main Panel
+    [[maybe_unused]] auto& mainPanel {panels.emplace_back(renderQueue)};
+    size_t mainPanelID {panels.size() - 1};
+
+    /********************************* Screen *********************************/
+
+    // Initialize Screen
+    auto& screen {screens.emplace_back()};
+
+    // Add Panel IDs
+    screen.panelIDs.emplace_back(mainPanelID);
+
+    // Set Screen Focus Panel
+    screen.currentPanelID = mainPanelID;
+}
+
+
+void ScreenManager::initMainMenu() /* Work In Progress */
+{
+    // Allocate Memory For Panels (Prevents Reference Invalidation)
+    uint32_t panelCount {1 /* + 3 */};
+    panels.reserve(panels.size() + panelCount);
+
+    /******************************* Main Panel *******************************/
+
+    // Initialize Main Panel
+    [[maybe_unused]] auto& mainPanel {panels.emplace_back(renderQueue)};
+    size_t mainPanelID {panels.size() - 1};
+
+    /*************************** Singleplayer Panel ***************************/
+
+    // Initialize Singleplayer Panel
+    // ...
+
+    /*************************** Multiplayer Panel ****************************/
+
+    // Initialize Multiplayer Panel
+    // ...
+
+    /***************************** Settings Panel *****************************/
+
+    // Initialize Settings Panel
+    // ...
+
+    /********************************* Screen *********************************/
+
+    // Initialize Screen
+    auto& screen {screens.emplace_back()};
+
+    // Add Panel IDs
+    screen.panelIDs.emplace_back(mainPanelID);
+
+    // Set Screen Focus Panel
+    screen.currentPanelID = mainPanelID;
+}
+
+
+void ScreenManager::initPacman() /* Work In Progress */
+{
+
+}
+
+/********************************** Getters ***********************************/
+
+Panel& ScreenManager::getCurrentScreenPanel()
+{
+    DEBUG_ASSERT(currentScreenID >= 2);
+    DEBUG_ASSERT(currentScreenID - 2 < screens.size());
+
+    uint32_t index {screens.at(currentScreenID - 2).currentPanelID};
+    return panels.at(index);
+};
+
+/******************************** Constructors ********************************/
+
+ScreenManager::ScreenManager(class RenderQueue* const renderQueue)
+    : renderQueue{renderQueue}
+{
+    DEBUG_ASSERT(renderQueue != nullptr);
 }
--- src/Screen/Manager/ScreenManager.h
+++ src/Screen/Manager/ScreenManager.h
@@ -2,6 +2,11 @@
 #define SRC_SCREEN_MANAGER_H
 
 
+#include "Panel.h"
+
+#include <vector>
+
+
 ////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Screen Manager ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -11,17 +16,57 @@ class ScreenManager
 {
 private:
 
+    /****************************** Data Structs ******************************/
+
+    struct Screen
+    {
+        std::vector<uint32_t> panelIDs; // ID represents Panel Vector Index
+        uint32_t currentPanelID;
+    };
+
+    /****************************** Raw Pointers ******************************/
+
+    class RenderQueue* renderQueue {};
+
     /******************************* Variables ********************************/
 
-    // ...
+    bool consoleActive {false};
+    uint32_t currentScreenID {};    // ID represents Screen Code
+    std::vector<Panel> panels {};
+    std::vector<Screen> screens {};
+
+    /************************** Init-Stage Functions **************************/
+
+    void initConsole();
+    void initMainMenu();
+    void initPacman();
+
+    /******************************** Getters *********************************/
+
+    /*
+        Return the focus panel for the current screen
+    */
+    Panel& getCurrentScreenPanel();
+
+    /******************************** Setters *********************************/
+
+    /*
+        Set the current screen to the specified screen ID
+    */
+    void setCurrentScreen(uint32_t id) {currentScreenID = id;}
 
 public:
 
     /***************************** Main Functions *****************************/
 
     void init();
-    void update();
+    uint32_t update();
+    void fillRenderQueue();
     void cleanup();
+
+    /****************************** Constructors ******************************/
+
+    ScreenManager(class RenderQueue* const renderQueue);
 };
 
 
